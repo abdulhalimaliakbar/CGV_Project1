07f039c3fe9ef3236035e731ad0b1824a3d212ae
jxcodetw <jxcode.tw@gmail.com>
2018-01-10 00:23:59 +0800

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Implmentation of A Deep Learning Approach for Generalized Speech Animation [Taylor et al. 2017]
+
 [paper](http://www.yisongyue.com/publications/siggraph2017_speech.pdf)
--- display/main.cpp
+++ display/main.cpp
@@ -17,17 +17,8 @@ _GLMmodel *mesh;
 int WindWidth, WindHeight;
 
 int last_x, last_y;
-int selectedFeature = -1;
-vector<int> featureList;
-
-vector3 *ctrlpoint;
-GLfloat *origin, **distances;
 
 const float epsilon = 1e-6;
-const float sig = 0.15;
-const int N = 30;
-float inv[N][N*2];
-vector3 weight[N];
 
 void Reshape(int width, int height)
 {
@@ -67,19 +58,6 @@ void Display(void)
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glmDraw(mesh, GLM_SMOOTH);
 
-	// render features
-	glPointSize(10.0);
-	glColor3f(1.0, 0.0, 0.0);
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POINTS);
-	for (int i = 0; i < featureList.size(); i++)
-	{
-		int idx = featureList[i];
-
-		glVertex3fv((float *)&mesh->vertices[3 * idx]);
-	}
-	glEnd();
-
 	glPopMatrix();
 
 	glFlush();
@@ -113,191 +91,28 @@ vector3 Unprojection(vector2 _2Dpos)
 	return vector3(wpos[0], wpos[1], wpos[2]);
 }
 
-GLfloat preventZero(GLfloat f)
-{
-	if (f == 0) {
-		return epsilon;
-	}
-	return f;
-}
-
-void addControlPoint(int idx)
-{
-	printf("%d\n", idx);
-	featureList.push_back(idx);
-
-	ctrlpoint[idx] = vector3(
-		preventZero(mesh->vertices[3 * idx + 0] - origin[3 * idx + 0]),
-		preventZero(mesh->vertices[3 * idx + 1] - origin[3 * idx + 1]),
-		preventZero(mesh->vertices[3 * idx + 2] - origin[3 * idx + 2]));
-}
-
 void mouse(int button, int state, int x, int y)
 {
 	tbMouse(button, state, x, y);
 
-	// add feature
-	if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
-	{
-		int minIdx = 0;
-		float minDis = 9999999.0f;
-
-		vector3 pos = Unprojection(vector2((float)x, (float)y));
-
-		for (int i = 0; i < mesh->numvertices; i++)
-		{
-			vector3 pt(mesh->vertices[3 * i + 0], mesh->vertices[3 * i + 1], mesh->vertices[3 * i + 2]);
-			float dis = (pos - pt).length();
-
-			if (minDis > dis)
-			{
-				minDis = dis;
-				minIdx = i;
-			}
-		}
-
-
-		if (std::find(featureList.begin(), featureList.end(), minIdx) == featureList.end())
-		{
-			addControlPoint(minIdx);
-		}
-
-	}
-
-	// manipulate feature
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-	{
-		int minIdx = 0;
-		float minDis = 9999999.0f;
-
-		vector3 pos = Unprojection(vector2((float)x, (float)y));
-
-		for (int i = 0; i < featureList.size(); i++)
-		{
-			int idx = featureList[i];
-			vector3 pt(mesh->vertices[3 * idx + 0], mesh->vertices[3 * idx + 1], mesh->vertices[3 * idx + 2]);
-			float dis = (pos - pt).length();
-
-			if (minDis > dis)
-			{
-				minDis = dis;
-				minIdx = featureList[i];
-			}
-		}
-
-		selectedFeature = minIdx;
-
-	}
-
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
-		selectedFeature = -1;
 
 	last_x = x;
 	last_y = y;
-}
-
-void calc_weight()
-{
-	// init
-	for (int i = 0; i < featureList.size(); ++i)
-	{
-		inv[i][i] = 1;
-		for (int j = i + 1; j < featureList.size(); ++j)
-		{
-			inv[j][i] = inv[i][j] = distances[featureList[i]][featureList[j]];
-		}
-		for (int j = 0; j < featureList.size(); ++j)
-		{
-			if (i == j)
-			{
-				inv[i][j+featureList.size()] = 1;
-			}
-			else
-			{
-				inv[i][j+featureList.size()] = 0;
-			}
-		}
-	}
-
-
-	// rref
-	for (int i = 0; i < featureList.size(); ++i)
-	{
-		float pivot = inv[i][i];
-		for (int j = i; j < 2 * featureList.size(); ++j)
-		{
-			inv[i][j] /= pivot;
-		}
-		for (int k = 0; k < featureList.size(); ++k)
-		{
-			if (i == k) { continue; }
-			float factor = inv[k][i];
-			for (int j = i; j < 2 * featureList.size(); ++j)
-			{
-				inv[k][j] -= inv[i][j] * factor;
-			}
-		}
-	}
-
-	for (int i = 0; i < featureList.size(); ++i)
-	{
-		int idx = featureList[i];
-		vector3 vi = ctrlpoint[idx];
-		vector3 wvi(0, 0, 0);
-		for (int j = 0; j < featureList.size(); ++j)
-		{
-			int idx = featureList[j];
-			wvi += inv[i][j + featureList.size()] * ctrlpoint[idx];
-		}
-
-		weight[i].x = wvi.x / vi.x;
-		weight[i].y = wvi.y / vi.y;
-		weight[i].z = wvi.z / vi.z;
-	}
-}
-
-void move_together()
-{
-	calc_weight();
-	memcpy(mesh->vertices, origin, sizeof(GLfloat) * mesh->numvertices * 3);
-	for (int i = 0; i < featureList.size(); i++)
-	{
-		vector3 vec = ctrlpoint[featureList[i]];
-		vec.x *= weight[i].x;
-		vec.y *= weight[i].y;
-		vec.z *= weight[i].z;
-		for (int j = 0; j < mesh->numvertices; j++)
-		{
-			float dis = distances[featureList[i]][j];
-
-			mesh->vertices[3 * j + 0] += dis * vec.x;
-			mesh->vertices[3 * j + 1] += dis * vec.y;
-			mesh->vertices[3 * j + 2] += dis * vec.z;
-		}
-	}
 }
 
 void motion(int x, int y)
 {
 	tbMotion(x, y);
-
-	if (selectedFeature != -1)
-	{
-		matrix44 m;
-		vector4 vec = vector4((float)(x - last_x) / 100.0f, (float)(y - last_y) / 100.0f, 0.0, 1.0);
-
-		gettbMatrix((float *)&m);
-		vec = m * vec;
-		vec /= 2;
-
-		ctrlpoint[selectedFeature].x += vec.x;
-		ctrlpoint[selectedFeature].y -= vec.y;
-		ctrlpoint[selectedFeature].z += vec.z;
-	}
-
-	move_together();
 	last_x = x;
 	last_y = y;
+}
+
+void Keyboard(unsigned char key, int x, int y) {
+	switch(key) {
+	case 27: // ESC
+		exit(0);
+		break;
+	}
 }
 
 void timf(int value)
@@ -309,8 +124,8 @@ void timf(int value)
 
 int main(int argc, char *argv[])
 {
-	WindWidth = 400;
-	WindHeight = 400;
+	WindWidth = 1024;
+	WindHeight = 768;
 
 	GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
 	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
@@ -319,13 +134,16 @@ int main(int argc, char *argv[])
 
 	glutInit(&argc, argv);
 	glutInitWindowSize(WindWidth, WindHeight);
+	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-WindWidth)/2,
+                       (glutGet(GLUT_SCREEN_HEIGHT)-WindHeight)/2);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-	glutCreateWindow("Trackball Example");
+	glutCreateWindow("Display Animation");
 
 	glutReshapeFunc(Reshape);
 	glutDisplayFunc(Display);
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
+	glutKeyboardFunc(Keyboard);
 	glClearColor(0, 0, 0, 0);
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
@@ -345,38 +163,13 @@ int main(int argc, char *argv[])
 	glutTimerFunc(40, timf, 0); // Set up timer for 40ms, about 25 fps
 
 	// load 3D model
+	std::cout << "Loading model ... ";
 	mesh = glmReadOBJ("./data/head.obj");
+	std::cout << "done." << std::endl;
 
 	glmUnitize(mesh);
 	glmFacetNormals(mesh);
 	glmVertexNormals(mesh, 90.0);
-
-	ctrlpoint = (vector3*)malloc(sizeof(vector3)*mesh->numvertices);
-	origin = (GLfloat*)malloc(sizeof(GLfloat) * mesh->numvertices * 3);
-	memcpy(origin, mesh->vertices, sizeof(GLfloat) * mesh->numvertices * 3);
-
-	distances = (GLfloat**)malloc(sizeof(GLfloat*) * mesh->numvertices);
-	for (int i = 0; i < mesh->numvertices; i++)
-	{
-		distances[i] = (GLfloat*)malloc(sizeof(GLfloat) * mesh->numvertices);
-	}
-	for (int i = 0; i < mesh->numvertices; i++)
-	{
-		// prevent inf
-		ctrlpoint[i] = vector3(0, 0, 0);
-
-		vector3 pi(origin[3 * i + 0], origin[3 * i + 1], origin[3 * i + 2]);
-		for (int j = 0; j < mesh->numvertices; j++)
-		{
-			vector3 pj(origin[3 * j + 0], origin[3 * j + 1], origin[3 * j + 2]);
-			float dis = (pi - pj).length();
-			distances[i][j] = distances[j][i] = exp(-dis*(dis / (2 * sig*sig)));
-		}
-	}
-
-	addControlPoint(990);
-	addControlPoint(2167);
-	//addControlPoint(751);
 	glutMainLoop();
 
 	return 0;
